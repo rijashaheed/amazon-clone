c11f94cc8336897ac6a2a5530ac223240e92c477
rijashaheed <56384906+rijashaheed@users.noreply.github.com>
2020-09-09 21:31:05 +0500

    Setup header and hero image

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="home__container">
+            <img
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg?fbclid=IwAR26S1X63b2uoB6qDb6Ohfvbv00q2sPTB-jTF3Y7RkNznE74hI4If-fLyD8"
+                alt="heroimg"
+            />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAROgJTrLDE1kNxXt3C-TurWMtQtPgp_Oc",
+    authDomain: "clone-28f1a.firebaseapp.com",
+    databaseURL: "https://clone-28f1a.firebaseio.com",
+    projectId: "clone-28f1a",
+    storageBucket: "clone-28f1a.appspot.com",
+    messagingSenderId: "606711814244",
+    appId: "1:606711814244:web:a3271e35c357c97c39a771",
+    measurementId: "G-T12VB2K8H9"
+  };
